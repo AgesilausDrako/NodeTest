dc1f814c6de28a6fb4e9eee93f0eb1c1f3aced43
AgesilausDrako <agesilaus@yahoo.com>
2017-12-07 15:32:48 -0500

    mouseover functionality working

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,11 +1,66 @@
-// const renderData = (results) => {
-//     const itemDisplay = document.querySelector("#sub-items");
-//     results.map((item, index) => {
-//         const productDisplay = item.networkPrice[index];
+const DETAIL_IMAGE_SELECTOR = "[data-image-role='target']";
+const DETAIL_TITLE_SELECTOR = "[data-title-role='title']";
+const DETAIL_BULLETS_SELECTOR = "[data-bullets-role='bullets']";
+const DETAIL_PRICE_SELECTOR = "[data-price-role='price']";
+const ITEM_LINK_SELECTOR = "[data-image-role='trigger']";
 
-//         itemDisplay.appendChild(productDisplay);
-//         console.log("got here");
-//     });
-// }
+const setDetails = (imageUrl, titleText, bullets, price) => {
+    'use strict';
+    const detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+  
+    const detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
 
-// renderData();
+    const detailBullets = document.querySelector(DETAIL_BULLETS_SELECTOR);
+    detailBullets.textContent = bullets;
+
+    const detailPrice = document.querySelector(DETAIL_PRICE_SELECTOR);
+    detailPrice.textContent = price;   
+}
+
+const imageFromItem = (item) => {
+    'use strict';
+    return item.getAttribute('data-image-url');
+}
+  
+const titleFromItem = (item) => {
+    'use strict';
+    return item.getAttribute('data-title-role')
+}
+
+const bulletsFromItem = (item) => {
+    'use strict';
+    return item.getAttribute('data-bullets-role')
+}
+
+const priceFromItem = (item) => {
+    'use strict';
+    return item.getAttribute('data-price-role')
+}
+
+const setDetailsFromItem = (item) => {
+    'use strict'
+    setDetails(imageFromItem(item), titleFromItem(item), bulletsFromItem(item), priceFromItem(item));
+}
+
+const addItemHoverHandler = (item) => {
+    item.addEventListener('mouseover', () => {
+        setDetailsFromItem(item);
+    });
+}
+
+const getItemsArray = () => {
+    'use strict';
+    const items = document.querySelectorAll(ITEM_LINK_SELECTOR);
+    const itemArray = [].slice.call(items);
+    return itemArray;
+}
+
+const initializeEvents = () => {
+    'use strict';
+    const items = getItemsArray();
+    items.forEach(addItemHoverHandler);
+}
+
+initializeEvents();
